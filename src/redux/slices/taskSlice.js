c38ac5b24fd905bb6bcf4b3e6a55aba4b62b7437
Clifin Cletus <clifin18@gmail.com>
2026-03-27 08:29:47 +0530

    feat:add task creation functionality with firestore

--- src/redux/slices/taskSlice.js
+++ src/redux/slices/taskSlice.js
@@ -1,10 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { db, auth } from "@/services/firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 const initialState = {
     tasks: [],
     loading: false,
     error: null,
 };
+
+export const createTask = createAsyncThunk(
+    "tasks/createTask",
+    async (taskData, thunkAPI) => {
+        try {
+            const user = auth.currentUser;
+
+            if (!user) {
+                throw new Error("User not authenticated");
+            }
+
+            const docRef = await addDoc(collection(db, "tasks"), {
+                ...taskData,
+                uid: user.uid,
+                createdAt: serverTimestamp(),
+            });
+
+            return {
+                id: docRef.id,
+                ...taskData,
+                uid: user.uid,
+            };
+        } catch (err) {
+            return thunkAPI.rejectWithValue(err.message);
+        }
+    }
+);
 
 const taskSlice = createSlice({
     name: "tasks",
@@ -16,6 +45,26 @@ const taskSlice = createSlice({
         addTaskLocal: (state, action) => {
             state.tasks.push(action.payload);
         },
+    },
+    extraReducers: (builder) => {
+        builder
+            //pending
+            .addCase(createTask.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+
+            // success
+            .addCase(createTask.fulfilled, (state, action) => {
+                state.loading = false;
+                state.tasks.push(action.payload);
+            })
+
+            // error
+            .addCase(createTask.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            });
     },
 });
 
